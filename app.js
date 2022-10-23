2634bfd36e510358ece01bbd610c962aaa7aa1ff
Ngoy banza <dngoyb@gmail.com>
2022-10-23 15:53:15 +0200

    CreateElement: function to help with elements creation

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('testing');
+import { fetchJSON } from './functions/api.js';
+import { createElement } from './functions/dom.js';
+
+try {
+	const todos = await fetchJSON(
+		'https://jsonplaceholder.typicode.com/posts?_limit=5'
+	);
+} catch (error) {
+	createElement('div', {
+		class: 'alert alert-danger',
+		role: 'alert',
+	});
+}
